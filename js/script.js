353c31dfbc9d8a75fca48558f6b19efd96d0c8db
Blake H <blake.hollingsworth80@gmail.com>
2023-06-08 11:26:45 -0600

    added jquery and dayjs as well as inserted some elements into the DOM from data

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-function getWeather() {
+var searchTextBox = $('#searchText')
+const searchButton = $('#searchButton')
+
+function populateCurrentWeather(data) {
+    let container = $('#currentWeather')
+
+    // create the header element
+    let heading = $('<h2>')
+    heading.text(data.name)
+    heading.addClass('currentWeatherTitle')
+    container.append(heading)
+
+    // create the date element
+    let date = $('<h3>')
+    date.text = (dayjs().format('dddd, MMMM D'))
+    container.append(date)
+}
+
+
+function getWeather(city) {
     let apiKey = 'e6dfd918c685c8022ca0666f1c5af0c6'
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&APPID=e6dfd918c685c8022ca0666f1c5af0c6`
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`
     fetch(url, {
         method: "GET"
     })
@@ -8,6 +27,11 @@ function getWeather() {
             return response.json();
         })
         .then(function (data) {
-            console.log(data.name);
+            console.log(data);
+            populateCurrentWeather(data)
         })
 };
+
+searchButton.on('click', function () {
+    getWeather(searchTextBox.val())
+})
